694f208d133a3f889c31f01bd1dfed9c29d8e9d7
Emrah Yücel <127504239+emrahYucel0@users.noreply.github.com>
2024-04-20 00:53:40 +0300

    Features with Mediator Design Pattern

---
+++ Core/CarBook.Application/Features/Mediator/Results/FeatureResults/GetFeatureByIdQueryResult.cs
@@ -0,0 +1,7 @@
+﻿namespace CarBook.Application.Features.Mediator.Results.FeatureResults;
+
+public class GetFeatureByIdQueryResult
+{
+    public int FeatureID { get; set; }
+    public string Name { get; set; }
+}
--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -6,6 +6,7 @@ using CarBook.Application.Features.CQRS.Handlers.CategoryHandler;
 using CarBook.Application.Features.CQRS.Handlers.ContactHandlers;
 using CarBook.Application.Interfaces;
 using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Application.Services;
 using CarBook.Persistence.Context;
 using CarBook.Persistence.Repositories;
 using CarBook.Persistence.Repositories.CarRepositories;
@@ -54,6 +55,8 @@ builder.Services.AddScoped<GetContactByIdQueryHandler>();
 builder.Services.AddScoped<CreateContactCommandHandler>();
 builder.Services.AddScoped<UpdateContactCommandHandler>();
 builder.Services.AddScoped<RemoveContactCommandHandler>();
+
+builder.Services.AddApplicationService(builder.Configuration);
 
 
 builder.Services.AddControllers();
