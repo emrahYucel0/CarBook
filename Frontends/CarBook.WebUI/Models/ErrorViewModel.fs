8193d6ba14f8cb994d6d13ca65ab5c1016ca1a00
Emrah Yücel <127504239+emrahYucel0@users.noreply.github.com>
2024-04-24 16:20:17 +0300

    Initialize Fronted

---
+++ Frontends/CarBook.WebUI/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace CarBook.WebUI.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
